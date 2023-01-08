359d4b29d537353e8981e659f739f104a4a4df66
AnXinXiao <1466775243@qq.com>
2023-01-09 00:26:57 +0800

    enum-RevisedPoker

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,53 @@
-function sum(a, b) {
-    return a + b;
+var Color;
+(function (Color) {
+    Color["heart"] = "\u2665";
+    Color["spade"] = "\u2660";
+    Color["club"] = "\u2663";
+    Color["diamond"] = "\u2666";
+})(Color || (Color = {}));
+var Mark;
+(function (Mark) {
+    Mark["A"] = "A";
+    Mark["TWO"] = "2";
+    Mark["THREE"] = "3";
+    Mark["FOUR"] = "4";
+    Mark["FIVE"] = "5";
+    Mark["SIX"] = "6";
+    Mark["SEVEN"] = "7";
+    Mark["EIGHT"] = "8";
+    Mark["NIGHT"] = "9";
+    Mark["TEN"] = "10";
+    Mark["ElEVEN"] = "J";
+    Mark["TWELVE"] = "Q";
+    Mark["KING"] = "K";
+    Mark["BIG"] = "Big King";
+    Mark["LITTLE"] = "Little King";
+})(Mark || (Mark = {}));
+function createDeck() {
+    const deck = [];
+    const marks = Object.values(Mark);
+    const colors = Object.values(Color);
+    for (const m of marks) {
+        for (const c of colors) {
+            deck.push({ color: c, mark: m });
+        }
+    }
+    return deck;
 }
-sum(1, 2);
-let num = sum(3, 4);
-let name = 'kevin';
-let age = 12;
-let phone = '13333333333';
+function printDeck(deck) {
+    let result = '\n';
+    deck.forEach((card, i) => {
+        let str = card.color + card.mark;
+        if (!(card.mark === Mark.BIG || card.mark === Mark.LITTLE)) {
+            result += str + '\t';
+            if ((i + 1) % 4 === 0) {
+                result += '\n';
+            }
+        }
+    });
+    result += Mark.BIG + '\t';
+    result += Mark.LITTLE + '\t';
+    console.log(result);
+}
+const deck = createDeck();
+printDeck(deck);
--- src/index.ts
+++ src/index.ts
@@ -1,131 +1,58 @@
-// function isOdd(n:number):boolean{
-//     return  n%2 !== 0
-// }
-// let nums:number[] = [3,4,5]
-
-// let nums:Array<number>=[3,4,5]
-
-// function printValues(obj:Object){
-//    const values = Object.values(obj)
-//    values.forEach(v=>console.log(v))
-// }
-
-// let name:string | undefined
-// name='倪好'
-// if(typeof name === "string"){
-//   console.log(name.charAt(1))
-// }
-//
-// let gender:"男" | "女"
-//
-// let arr:[];
-//
-// let user:{
-//   name:string,
-//   age:number
-// }
-
-// let tu:[string,number]
-
-// let a
-// a=null
-
-// let data:any = "any"
-// let num:number = data
-// console.log(num)
-
-// type User = {
-//     name:string,
-//     age:number,
-//     sex:'男'|'女'
-// }
-//
-// type Gender = '男' | '女'
-//
-// let user:User;
-//
-//
-// function getUsers(g:Gender):User[]{
-//     return [
-//         {name:'倪好',age:1,sex:g}
-//         ]
-// }
-//
-// /**
-//  * 得到 a*b 的结果
-//  * @param a
-//  * @param b
-//  */
-// function combine(a:number,b:number):number;
-// /**
-//  * 得到 a，b拼接结果
-//  * @param a
-//  * @param b
-//  */
-// function combine(a:string,b:string):string
-// function combine(a:number|string,b:number|string):number|string{
-//     if(typeof a === 'number' && typeof  b === "number"){
-//         return a*b
-//     }else if(typeof a=== 'string' && typeof b === 'string'){
-//         return a+b
-//     }
-//     throw new Error('a，b必须是相同类型')
-// }
-// console.log(combine('3','3'))
-
-// function  sum(a:number,b:number,c:number=0):number{
-//         return a+b+c
-// }
-// console.log(sum(1,2))
-
-
 type Deck = NormalCard[]
-type Color = '♥' | '♠' | '♦' | '♣' | 'red King' | 'black King'
+enum Color {
+    heart ="♥",
+    spade = "♠",
+    club = "♣",
+    diamond="♦",
+}
+enum Mark{
+    A="A",
+    TWO="2",
+    THREE="3",
+    FOUR="4",
+    FIVE="5",
+    SIX="6",
+    SEVEN="7",
+    EIGHT="8",
+    NIGHT="9",
+    TEN="10",
+    ElEVEN="J",
+    TWELVE="Q",
+    KING = 'K',
+    BIG='Big King',
+    LITTLE='Little King'
+}
 type  NormalCard = {
     color: Color,
-    mark: number | 'Big King' | 'Little King'
+    mark: Mark
 }
 
 function createDeck(): Deck {
     const deck: Deck = [];
-    for (let i = 1; i <= 13; i++) {
-        deck.push(
-            {mark: i, color: '♥'},
-            {mark: i, color: '♠'},
-            {mark: i, color: '♦'},
-            {mark: i, color: '♣'}
-        )
+   const marks = Object.values(Mark)
+    const colors = Object.values(Color)
+    for (const m of marks) {
+        for (const c of colors) {
+            deck.push({color:c,mark:m})
+        }
     }
-    deck.push({color: 'red King', mark: 'Big King'}, {color: 'black King', mark: 'Little King'})
     return deck
 }
 
 function printDeck(deck: Deck) {
     let result = '\n';
     deck.forEach((card,i) => {
-        let str = card.color
-        if (card.mark>1 && card.mark <= 10) {
-            str += card.mark
-        }else if (card.mark === 1){
-            str+='A'
-        }
-        else if(card.mark === 11){
-            str+='J'
-        }else if(card.mark === 12){
-            str+='Q'
-        }else if(card.mark === 13){
-            str+='K'
-        }
-      result+=str+'\t';
-        if((i+1)%4 === 0){
-            result += '\n'
+        let str = card.color + card.mark
+        if(!(card.mark === Mark.BIG || card.mark === Mark.LITTLE)){
+            result+=str + '\t';
+            if((i+1) % 4 ===0){
+                result+= '\n'
+            }
         }
     })
+    result+=Mark.BIG+'\t';
+    result+=Mark.LITTLE+'\t';
     console.log(result)
 }
 const deck = createDeck()
 printDeck(deck)
-
-
-
-
