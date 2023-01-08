6b5e20efd1a8280a2ec4486823de78c4eb08ee9c
AnxinXiao <116351268+AnxinXiao@users.noreply.github.com>
2023-01-09 01:56:16 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,59 @@
-type Deck = NormalCard[]
-type Color = '♥' | '♠' | '♦' | '♣' | 'red' | 'black'
-type  NormalCard = {
-    color: Color,
-    mark: number | 'Big King' | 'Little King'
+type Deck = NormalCard[];
+enum Color {
+  heart = "♥",
+  spade = "♠",
+  club = "♣",
+  diamond = "♦",
 }
+enum Mark {
+  A = "A",
+  TWO = "2",
+  THREE = "3",
+  FOUR = "4",
+  FIVE = "5",
+  SIX = "6",
+  SEVEN = "7",
+  EIGHT = "8",
+  NIGHT = "9",
+  TEN = "10",
+  ElEVEN = "J",
+  TWELVE = "Q",
+  KING = "K",
+  BIG = "Big King",
+  LITTLE = "Little King",
+}
+type NormalCard = {
+  color: Color;
+  mark: Mark;
+};
 
 function createDeck(): Deck {
-    const deck: Deck = [];
-    for (let i = 1; i <= 13; i++) {
-        deck.push(
-            {mark: i, color: '♥'},
-            {mark: i, color: '♠'},
-            {mark: i, color: '♦'},
-            {mark: i, color: '♣'}
-        )
+  const deck: Deck = [];
+  const marks = Object.values(Mark);
+  const colors = Object.values(Color);
+  for (const m of marks) {
+    for (const c of colors) {
+      deck.push({ color: c, mark: m });
     }
-    deck.push({color: 'red', mark: 'Big King'}, {color: 'black', mark: 'Little King'})
-    return deck
+  }
+  return deck;
 }
 
 function printDeck(deck: Deck) {
-    let result = '\n';
-    deck.forEach((card,i) => {
-        let str = card.color
-        if (card.mark>1 && card.mark <= 10) {
-            str += card.mark
-        }else if (card.mark === 1){
-            str+='A'
-        }
-        else if(card.mark === 11){
-            str+='J'
-        }else if(card.mark === 12){
-            str+='Q'
-        }else if(card.mark === 13){
-            str+='K'
-        }
-
-        if(!(card.color === "red" || card.color === "black")){
-            result+=str+'\t';
-        }else{
-            result+=card.mark+'\t'
-        }
-        if((i+1)%4 === 0){
-            result += '\n'
-        }
-    })
-    console.log(result)
+  let result = "\n";
+  deck.forEach((card, i) => {
+    let str = card.color + card.mark;
+    if (!(card.mark === Mark.BIG || card.mark === Mark.LITTLE)) {
+      result += str + "\t";
+      if ((i + 1) % 4 === 0) {
+        result += "\n";
+      }
+    }
+  });
+  result += Mark.BIG + "\t";
+  result += Mark.LITTLE + "\t";
+  console.log(result);
 }
-const deck = createDeck()
-printDeck(deck)
-
+const deck = createDeck();
+printDeck(deck);
 
